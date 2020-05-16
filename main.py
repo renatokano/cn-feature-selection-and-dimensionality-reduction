17e965ab89c2fe52c4fbe79f1bf5cc7dfeb54435
renatokano <renatokano16@gmail.com>
2020-05-16 06:25:00 -0300

    Add: challenge's solution

---
+++ main.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Desafio 5
+# 
+# Neste desafio, vamos praticar sobre redução de dimensionalidade com PCA e seleção de variáveis com RFE. Utilizaremos o _data set_ [Fifa 2019](https://www.kaggle.com/karangadiya/fifa19), contendo originalmente 89 variáveis de mais de 18 mil jogadores do _game_ FIFA 2019.
+# 
+# > Obs.: Por favor, não modifique o nome das funções de resposta.
+
+# ## _Setup_ geral
+
+# In[77]:
+
+
+from math import sqrt
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as sct
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.stats as st
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LinearRegression
+
+from loguru import logger
+
+
+# In[ ]:
+
+
+# Algumas configurações para o matplotlib.
+#%matplotlib inline
+
+from IPython.core.pylabtools import figsize
+
+
+figsize(12, 8)
+
+sns.set()
+
+
+# In[ ]:
+
+
+fifa = pd.read_csv("fifa.csv")
+
+
+# In[ ]:
+
+
+columns_to_drop = ["Unnamed: 0", "ID", "Name", "Photo", "Nationality", "Flag",
+                   "Club", "Club Logo", "Value", "Wage", "Special", "Preferred Foot",
+                   "International Reputation", "Weak Foot", "Skill Moves", "Work Rate",
+                   "Body Type", "Real Face", "Position", "Jersey Number", "Joined",
+                   "Loaned From", "Contract Valid Until", "Height", "Weight", "LS",
+                   "ST", "RS", "LW", "LF", "CF", "RF", "RW", "LAM", "CAM", "RAM", "LM",
+                   "LCM", "CM", "RCM", "RM", "LWB", "LDM", "CDM", "RDM", "RWB", "LB", "LCB",
+                   "CB", "RCB", "RB", "Release Clause"
+]
+
+try:
+    fifa.drop(columns_to_drop, axis=1, inplace=True)
+except KeyError:
+    logger.warning(f"Columns already dropped")
+
+
+# ## Inicia sua análise a partir daqui
+
+# In[8]:
+
+
+fifa.head()
+
+
+# In[10]:
+
+
+fifa.shape
+
+
+# In[11]:
+
+
+fifa.isna().sum()
+
+
+# In[42]:
+
+
+fifa.dropna(inplace=True)
+fifa.describe()
+
+
+# ## Questão 1
+# 
+# Qual fração da variância consegue ser explicada pelo primeiro componente principal de `fifa`? Responda como um único float (entre 0 e 1) arredondado para três casas decimais.
+
+# In[44]:
+
+
+def q1():
+    # linear dimensionality reduction using SVD
+    # fit the 'fifa' model w/o 1 component
+    pca = PCA(n_components=1).fit(fifa)
+    # get the percentage of variance explained by the selected components (in this case, w/j the 1st component)
+    explained_variance_ratio = float(pca.explained_variance_ratio_)
+    return round(explained_variance_ratio, 3)
+
+
+# In[45]:
+
+
+q1()
+
+
+# ## Questão 2
+# 
+# Quantos componentes principais precisamos para explicar 95% da variância total? Responda como un único escalar inteiro.
+
+# In[54]:
+
+
+
+
+
+# In[56]:
+
+
+def q2():
+    # select the number of components such that the amount of variance that needs to be explained is greater than the 0.95 specified by n_components
+    my_model = PCA(n_components=0.95, svd_solver='full').fit(fifa)
+    # the estimated number of components
+    n_components = my_model.n_components_
+    return int(n_components)
+
+
+# In[57]:
+
+
+q2()
+
+
+# ## Questão 3
+# 
+# Qual são as coordenadas (primeiro e segundo componentes principais) do ponto `x` abaixo? O vetor abaixo já está centralizado. Cuidado para __não__ centralizar o vetor novamente (por exemplo, invocando `PCA.transform()` nele). Responda como uma tupla de float arredondados para três casas decimais.
+
+# In[62]:
+
+
+x = [0.87747123,  -1.24990363,  -1.3191255, -36.7341814,
+     -35.55091139, -37.29814417, -28.68671182, -30.90902583,
+     -42.37100061, -32.17082438, -28.86315326, -22.71193348,
+     -38.36945867, -20.61407566, -22.72696734, -25.50360703,
+     2.16339005, -27.96657305, -33.46004736,  -5.08943224,
+     -30.21994603,   3.68803348, -36.10997302, -30.86899058,
+     -22.69827634, -37.95847789, -22.40090313, -30.54859849,
+     -26.64827358, -19.28162344, -34.69783578, -34.6614351,
+     48.38377664,  47.60840355,  45.76793876,  44.61110193,
+     49.28911284
+]
+
+
+# In[73]:
+
+
+def q3():
+    # linear dimensionality reduction using SVD
+    # fit the model w/o 2 components
+    pca = PCA(n_components=2).fit(fifa)
+    # generate a new basis (note: the principal components are orthogonal)
+    basis = pca.components_
+    # finding the coordinate of the vector 'x' with respect to a new basis
+    projection = basis.dot(x)
+    return tuple(projection.round(3)) 
+
+
+# In[76]:
+
+
+q3()
+
+
+# ## Questão 4
+# 
+# Realiza RFE com estimador de regressão linear para selecionar cinco variáveis, eliminando uma a uma. Quais são as variáveis selecionadas? Responda como uma lista de nomes de variáveis.
+
+# In[96]:
+
+
+def q4():
+    # RFE - Recursive Feature Elimination w/ linear regression
+    # get target variable: 'Overall'
+    y = fifa['Overall']
+    # dataset w/only independent variables (inputs)
+    x = fifa.drop(['Overall'], axis=1)
+    # use linear regression as the model
+    lr = LinearRegression()
+    # rfe w/ linear regression
+    rfe = RFE(lr, n_features_to_select=5).fit(x,y)
+    # get the selected features
+    selected_features = x.loc[:,rfe.support_].columns
+    # convert to list
+    result = list(selected_features)
+    return result
+
+
+# In[97]:
+
+
+q4()
+
